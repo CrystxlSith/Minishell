e619226d42955bb697c0520981b2b523c62b1f76
CrystalSith <joaquim1810@gmail.com>
2024-09-04 11:20:16 +0200

    token logic

--- token/token.c
+++ token/token.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 10:01:37 by kali              #+#    #+#             */
-/*   Updated: 2024/09/04 07:52:07 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/09/04 11:17:59 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,19 +73,15 @@ static int	check_tokens(char **str, t_token **tokens)
 	else if (**str == '>')
 	{
 		if (*(*str + 1) == '>')
-			return (new_token(tokens, create_new_token(E_REDIR_APP, ">>")), (*str)++, 1);
-		return (new_token(tokens, create_new_token(E_REDIR_OUT, ">")), 1);
+			return (new_token(tokens, create_new_token(E_REDIR_APP, ">>")), (*str) += 2, 1);
+		return (new_token(tokens, create_new_token(E_REDIR_OUT, ">")), (*str)++, 1);
 	}
 	else if (**str == '<')
 	{
 		if (*(*str + 1) == '<')
-			return (new_token(tokens, create_new_token(E_REDIR_DEL, "<<")), (*str)++, 1);
-		return (new_token(tokens, create_new_token(E_REDIR_IN, "<")), 1);
+			return (new_token(tokens, create_new_token(E_REDIR_DEL, "<<")), (*str) += 2, 1);
+		return (new_token(tokens, create_new_token(E_REDIR_IN, "<")), (*str)++, 1);
 	}
-	else if (**str == '>')
-		return (new_token(tokens, create_new_token(E_REDIR_OUT, ">")), 1);
-	// else if (**str == ' ')
-	// 	return (new_token(tokens, create_new_token(E_SPACES, " ")), 1);
 	else if (**str == '\'' || **str == '"')
 		return (quotes_handler(tokens, str), 1);
 	return (0);
@@ -106,25 +102,24 @@ static void	check_cmd(char **str, t_token **tokens)
 	}
 	cmd[i] = '\0';
 	new_token(tokens, create_new_token(E_CMD, ft_strdup(cmd)));
-	 if (ft_strchr(IS_TOKEN, **str))
-	 	check_tokens(str, tokens);
 }
 
 // Tokenize the input string
 t_token	*tokenize(char *str)
 {
 	t_token *tokens = NULL;
-	int	i;
 
-	i = 0;
 	while (*str)
 	{
 		while (ft_isspace(*str))
 			str++;
-		check_tokens(&str, &tokens);
 		if (ft_isalpha(*str))
 			check_cmd(&str, &tokens);
-		str++;
+		if (check_tokens(&str, &tokens))
+			continue ;
+		else
+			str++;
+
 	}
 	print_tokens(tokens);
 	return (tokens);
