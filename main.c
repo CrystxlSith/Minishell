af7e4ab973dd6ba44354e9bcac04c3ccf745897e
CrystalSith <joaquim1810@gmail.com>
2024-09-05 16:37:19 +0200

    p

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 10:33:26 by jopfeiff          #+#    #+#             */
-/*   Updated: 2024/09/05 15:39:38 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:28:13 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,14 @@ void	free_tokens(t_lexer *head)
 	}
 }
 
-int main(int argc, char const *argv[], char const *env[])
+int main(/*int argc, char const *argv[], char const *env[]*/)
 {
 	t_minishell	minishell;
 	t_lexer		*tokens;
 
-	if (argc || argv || env)
-		ft_memset(&minishell ,0 , sizeof(t_minishell));
-	
+	// if (argc || argv || env)
+	// 	ft_memset(&minishell ,0 , sizeof(t_minishell));
+	tokens = malloc(sizeof(t_lexer));
 	while (1)
 	{
 		// Retourne la ligne entree dans le terminal et ecris un prompt voulu
@@ -49,11 +49,14 @@ int main(int argc, char const *argv[], char const *env[])
 		printf("Exécution d'une commande...\n");
 		// Tokenize la commande
 		tokens = tokenize(minishell.line_read);
-		free_tokens(tokens);
 		// Indique à readline que nous sommes sur une nouvelle ligne
 		rl_on_new_line();
 		// rl_redisplay();  // Rafraîchit l'affichage du prompt
-		free(minishell.line_read);
+		if (!ft_strncmp(minishell.line_read, "exit", ft_strlen("exit")))
+			break ;
 	}
+	rl_clear_history();
+	free(minishell.line_read);
+	free_tokens(tokens);
 	return 0;
 }
